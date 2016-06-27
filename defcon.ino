c48a8f54cbcdf3d0f64d5f027293cc8c1d6d5546
Filip Adamczyk <Filip.Adamczyk@icrossing.com>
2016-06-26 19:56:12 -0700

    Update program with new light test and api hooks

--- defcon.ino
+++ defcon.ino
@@ -1,82 +1,50 @@
-int led1 = D6;
-int led2 = D5;
-int led3 = D4;
-int led4 = D3;
-int led5 = D2;
-
-
-// called once on startup
-void setup() {
-    pinMode(led1, OUTPUT);
-    pinMode(led2, OUTPUT);
-    pinMode(led3, OUTPUT);
-    pinMode(led4, OUTPUT);
-    pinMode(led5, OUTPUT);
-
-    // Turn off LEDs 1-5
-    lightsOff();
-    
-    Serial.begin(9600);
-    
-    // Run light test
-    lightTest();
-
-    // Lets listen for the hook response
-    Serial.println("Requesting Temperature!");
-
-    // Listening for the hook response
-    Spark.subscribe("hook-response/get_temp", temperatureData, MY_DEVICES);
-}
-
-
-// called forever every 5 minutes
-void loop() {
-    // Let's request the weather, but no more than once every 5 minutes.
-    Serial.println("Requesting Temperature!");
-
-    // publish the event that will trigger our Webhook
-    Spark.publish("get_temp");
-
-    // and wait at least 5 minutes before doing it again
-    delay(300000);
-}
+int led1 = D4; //white #1
+int led2 = D3; //red #2
+int led3 = D2; // yellow #3
+int led4 = D1; // green #4
+int led5 = D0; // blue #5
 
 // This function will get called when weather data comes in
-void temperatureData(const char *name, const char *data) {
-    double temp = strtod(data,NULL);
-    
-    Serial.println(temp);
-    
-    if (temp < 60) {
-        lightsOff();
-        digitalWrite(led1, HIGH);
-    } else if (temp <= 65) {
-        lightsOff();
-        digitalWrite(led2, HIGH);
-    } else if (temp <= 69) {
-        lightsOff();
-        digitalWrite(led3, HIGH);
-    } else if (temp <= 75) {
-        lightsOff();
-        digitalWrite(led4, HIGH);
-    } else {
-        lightsOff();
-        digitalWrite(led5, HIGH);
-    }
+void temperatureData(const char *event, const char *data) {
+  double temp = strtod(data,NULL);
+
+  Serial.println(temp);
+
+  if (temp <= 69.9) {
+    lightsOff();
+    digitalWrite(led5, HIGH);
+  } else if (temp <= 79.9) {
+    lightsOff();
+    digitalWrite(led4, HIGH);
+  } else if (temp <= 89.9) {
+    lightsOff();
+    digitalWrite(led3, HIGH);
+  } else if (temp <= 99.9) {
+    lightsOff();
+    digitalWrite(led2, HIGH);
+  } else {
+    lightsOff();
+    digitalWrite(led1, HIGH);
+  }
 }
 
 void lightTest() {
-  // To blink the LED, first we'll turn it on...
-  digitalWrite(led1, HIGH);
-  delay(500);
-  digitalWrite(led2, HIGH);
-  delay(500);
-  digitalWrite(led3, HIGH);
-  delay(500);
-  digitalWrite(led4, HIGH);
-  delay(500);
   digitalWrite(led5, HIGH);
+  delay(150);
+  digitalWrite(led5, LOW);
+  digitalWrite(led4, HIGH);
+  delay(150);
+  digitalWrite(led4, LOW);
+  digitalWrite(led3, HIGH);
+  delay(150);
+  digitalWrite(led3, LOW);
+  digitalWrite(led2, HIGH);
+  delay(150);
+  digitalWrite(led2, LOW);
+  digitalWrite(led1, HIGH);
+  delay(150);
 
+  lightsOn();
   // We'll leave it on for 1 second...
   delay(1000);
 
@@ -85,9 +53,68 @@ void lightTest() {
 }
 
 void lightsOff() {
-    digitalWrite(led1, LOW);
+  digitalWrite(led1, LOW);
+  digitalWrite(led2, LOW);
+  digitalWrite(led3, LOW);
+  digitalWrite(led4, LOW);
+  digitalWrite(led5, LOW);
+}
+
+void lightsOn() {
+  digitalWrite(led1, HIGH);
+  digitalWrite(led2, HIGH);
+  digitalWrite(led3, HIGH);
+  digitalWrite(led4, HIGH);
+  digitalWrite(led5, HIGH);
+}
+
+// called once on startup
+void setup() {
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
+  pinMode(led5, OUTPUT);
+
+  // Turn off LEDs 1-5
+  lightsOff();
+
+  Serial.begin(9600);
+
+  delay(5000);
+
+  // Run light test
+  lightTest();
+
+  // Listening for the hook response
+  Particle.subscribe("hook-response/get_temp", temperatureData , MY_DEVICES);
+}
+
+void loop() {
+  // Let's request the temperature, but no more than once every 5 minutes.
+  Serial.println("Requesting Temperature!");
+
+  // publish the event that will trigger our Webhook
+  bool success;
+  success = Particle.publish("get_temp");
+  if (!success) {
+    // get here if event publish did not work
     digitalWrite(led2, LOW);
-    digitalWrite(led3, LOW);
-    digitalWrite(led4, LOW);
-    digitalWrite(led5, LOW);
+    delay(100);
+    digitalWrite(led2, HIGH);
+    delay(100);
+    digitalWrite(led2, LOW);
+    delay(100);
+    digitalWrite(led2, HIGH);
+    delay(100);
+    digitalWrite(led2, LOW);
+    delay(100);
+    digitalWrite(led2, HIGH);
+    delay(100);
+    digitalWrite(led2, LOW);
+    delay(100);
+  }
+
+  // and wait at least 5 minutes before doing it again
+  delay(300000);
 }
